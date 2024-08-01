7b96efb260d3feeb21738ec74af950508a05ddaf
Yury Saukou <yury_saukou@epam.com>
2024-08-01 23:23:09 +0400

    add hidden navigation to login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,51 @@
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
+import { useCallback, useRef } from 'react';
 
+import { useRouter } from '@app/navigation';
+import Button from './Button';
 import ContentHolder from './ContentHolder';
-import LayoutSwitcher from './LayoutSwitcher/LayoutSwitcher';
-
-import logo from '../../public/logo.svg?url';
+import { LayoutSwitcher } from './LayoutSwitcher';
 import LocaleSwitcher from './LocaleSwitcher';
 
+import logo from '../../public/logo.svg?url';
+
 export default function Header() {
-  const translation = useTranslations('Header');
+  const router = useRouter();
+  const translation = useTranslations('common');
+
+  const holdTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const startHold = useCallback(() => {
+    holdTimeout.current = setTimeout(() => {
+      const shouldRedirect = confirm(translation('redirectToLoginConfirmMessage'));
+      
+      shouldRedirect && router.push('/login');
+    }, 2000);
+  }, [router, translation]);
+
+  const endHold = useCallback(() => {
+    if (holdTimeout.current) {
+      clearTimeout(holdTimeout.current);
+      holdTimeout.current = null;
+    }
+  }, []);
 
   return (
     <header className="p-4 flex justify-center items-center">
       <ContentHolder className="flex justify-between items-center">
-        <Image
-          priority
-          src={logo}
-          alt={translation('logoAlt')}
-        />
+        <Button
+          onMouseDown={startHold}
+          onMouseUp={endHold}
+          onMouseLeave={endHold}
+        >
+          <Image
+            priority
+            src={logo}
+            alt={translation('logoAlt')}
+            draggable="false"
+          />
+        </Button>
 
         <section className="flex gap-2 items-center">
           <LayoutSwitcher />
--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,13 +1,12 @@
 import { ReactNode } from 'react';
 
 import Header from './Header';
-import LocaleSwitcher from './LocaleSwitcher';
 
 type Props = {
   children?: ReactNode;
 };
 
-export default function PageLayout({children}: Props) {
+export default function PageLayout({ children }: Props) {
   return (
     <>
       <Header />
