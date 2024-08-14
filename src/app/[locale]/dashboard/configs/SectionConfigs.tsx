dacc7264a5ec1e2e79ea560d9da3c93940316b36
Yury Saukou <yury_saukou@epam.com>
2024-08-14 18:25:33 +0400

    updates

---
+++ src/app/[locale]/dashboard/configs/SectionConfigs.tsx
@@ -0,0 +1,122 @@
+'use client'
+
+import { Button, Form, FormProps, message, Select } from "antd";
+import { get } from 'lodash-es';
+import { useLocale, useTranslations } from "next-intl";
+import { useForm, Controller } from 'react-hook-form';
+
+import { aboutUsFieldNames } from "@app/constants";
+import { defaultLocale, locales } from "@app/navigation";
+import { upsertConfigs } from "@app/actions";
+
+type FormValues = {
+  TagFilters: string[],
+};
+
+interface Props {
+  configs: AppConfigs;
+  tags: Tag[];
+}
+
+const defaultOptionValue = 'isRecommended';
+const defaultOption = { value: defaultOptionValue, label: '🤤', disabled: true };
+
+const getTagsOptions = (tags: Tag[], currentLocale: Locale) => [
+  defaultOption,
+  ...tags.map(({ Name, TagId }) => ({ value: TagId, label: Name[currentLocale] })),
+];
+
+export default function SectionConfigs({ configs, tags }: Props) {
+  const { TagFilters } = configs;
+  const currentLocale = useLocale() as Locale;
+
+  const { handleSubmit, control, formState, reset } = useForm<FormValues>({
+    defaultValues: {
+      TagFilters: TagFilters ? [defaultOptionValue, ...TagFilters] : [defaultOptionValue],
+    }
+  });
+  const translation = useTranslations();
+
+  const formErrors = formState.errors;
+  const isDirty = formState.isDirty;
+
+  const onFinish: FormProps<FormValues>['onFinish'] = async (values) => {
+    const formValues = {
+      ...values,
+      TagFilters: values.TagFilters?.filter((tagId) => tagId !== defaultOptionValue),
+    };
+  
+    const data = {
+      ...configs,
+      ...formValues,
+    };
+
+    const actionMessageType = configs.ConfigId ? 'update' : 'create';
+
+    try {
+      const result = await upsertConfigs(data, { revalidatePaths: ['/configs', '/tags'] });
+
+      reset(values);
+
+      message.success(translation(
+        `Dashboard.section.message.${actionMessageType}.success`,
+        { entity: translation('common.entity.configs') },
+      ));
+    } catch (error) {
+      message.error(translation(
+        `Dashboard.section.message.${actionMessageType}.error`,
+        { entity: translation('common.entity.configs') },
+      ));
+    };
+  }
+
+  return (
+    <>
+      <h1 className="text-color-primary text-3xl font-semibold my-6">{translation(`Dashboard.section.configs.title`)}</h1>
+
+      <div className="p-6 bg-white rounded-xl">
+        <Form
+          onFinish={handleSubmit(onFinish)}
+        >
+          <div className="flex flex-col items-center gap-10 max-w-max">
+            <Form.Item
+              label={<span className="flex self-center text-lg font-semibold">{translation(`Dashboard.section.configs.tagsFilter`)}</span>}
+            >
+              <Controller
+                name="TagFilters"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    mode="tags"
+                    options={getTagsOptions(tags, currentLocale)}
+                    {...field}
+                  />
+                )}
+              />
+            </Form.Item>
+          </div>
+
+          <footer className="flex justify-end items-center">
+            <Form.Item>
+              <Button
+                type="link"
+                htmlType="reset"
+                className="mr-4"
+                disabled={!isDirty}
+              >
+                {translation('form.action.cancel')}
+              </Button>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!isDirty}
+              >
+                {translation('form.action.submit')}
+              </Button>
+            </Form.Item>
+          </footer>
+        </Form>
+      </div>
+    </>
+  )
+}
--- src/app/[locale]/dashboard/configs/page.tsx
+++ src/app/[locale]/dashboard/configs/page.tsx
@@ -1,67 +1,15 @@
-'use client'
+import { getAllTags, getConfigs } from "@app/actions";
 
-import { Button, Form, FormProps, Input, TimePicker } from "antd";
-import { get } from 'lodash-es';
-import { useTranslations } from "next-intl";
-import { ReactNode } from "react";
-import { useForm, Controller } from 'react-hook-form';
+import SectionConfigs from "./SectionConfigs";
 
-import { aboutUsFieldNames } from "@app/constants";
-import { defaultLocale, locales } from "@app/navigation";
-
-type FormValues = {
-};
-
-export default function SectionConfigs() {
-  const { handleSubmit, control, formState, getValues } = useForm<FormValues>({ defaultValues: {
-    [aboutUsFieldNames.workingHours]: null,
-    [aboutUsFieldNames.address]: locales.reduce((acc, curr) => ({ ...acc, [curr]: '' }), {}),
-    [aboutUsFieldNames.instagram]: '',
-  } });
-  const translation = useTranslations();
-
-  const formErrors = formState.errors;
-  const isDirty = formState.isDirty;
-
-  const onFinish: FormProps<FormValues>['onFinish'] = async (values) => {
-    // TODO: request to BE
-    console.log(values);
-  };
+export default async function AboutUsPage() {
+  const configs = await getConfigs().catch(() => ({}));
+  const tags = await getAllTags();
 
   return (
-    <>
-      <h1 className="text-color-primary text-3xl font-semibold my-6">{translation(`Dashboard.section.configs.title`)}</h1>
-
-      <div className="p-6 bg-white rounded-xl">
-        <Form
-          layout="vertical"
-          onFinish={handleSubmit(onFinish)}
-        >
-          <div className="flex flex-col gap-10 max-w-max">
-            123
-          </div>
-
-          <footer className="flex justify-end items-center">
-            <Form.Item>
-              <Button
-                type="link"
-                htmlType="reset"
-                className="mr-4"
-                disabled={!isDirty}
-              >
-                {translation('form.action.cancel')}
-              </Button>
-              <Button
-                type="primary"
-                htmlType="submit"
-                disabled={!isDirty}
-              >
-                {translation('form.action.submit')}
-              </Button>
-            </Form.Item>
-          </footer>
-        </Form>
-      </div>
-    </>
+    <SectionConfigs
+      configs={configs}
+      tags={tags}
+    />
   )
 }
